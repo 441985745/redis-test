6fd8fae1dbc6235b3bf5dfd986efab64b55aad0a
zhangshulong <441985745@qq.com>
2018-01-19 09:18:45 +0800

    整合freemarker模板

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: zhangshulong
+ * @create: 2018-01-19 09:07
+ **/
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello_world")
+    public ModelAndView hello(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("test");
+        mv.addObject("userName","大家");
+        return mv;
+    }
+}
